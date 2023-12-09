44695488c8580e433bfe2d7806223b54a612db12
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-12-09 08:42:17 +0100

    Move parser from previous repo

---
+++ parser/__tests__/index.test.js
@@ -0,0 +1,343 @@
+require('../out.js');
+
+const SAME = Symbol('same');
+
+expect.extend({
+  toBeParsedAs(received, expectedText, expectedRanges) {
+    const [actualText, actualRanges] =
+      global.parseMarkdownToTextAndRanges(received);
+    if (expectedText === SAME) {
+      expectedText = received;
+    }
+    if (actualText !== expectedText) {
+      return {
+        pass: false,
+        message: () =>
+          `Expected ${JSON.stringify(expectedText)}, got ${JSON.stringify(
+            actualText
+          )}`,
+      };
+    }
+    if (JSON.stringify(actualRanges) !== JSON.stringify(expectedRanges)) {
+      return {
+        pass: false,
+        message: () =>
+          `Expected ${JSON.stringify(expectedRanges)}, got ${JSON.stringify(
+            actualRanges
+          )}`,
+      };
+    }
+    return { pass: true };
+  },
+});
+
+test('empty string', () => {
+  expect('').toBeParsedAs(SAME, []);
+});
+
+test('no formatting', () => {
+  expect('Hello, world!').toBeParsedAs(SAME, []);
+});
+
+test('bold', () => {
+  expect('Hello, *world*!').toBeParsedAs(SAME, [
+    ['syntax', 7, 1],
+    ['bold', 8, 5],
+    ['syntax', 13, 1],
+  ]);
+});
+
+test('italic', () => {
+  expect('Hello, _world_!').toBeParsedAs(SAME, [
+    ['syntax', 7, 1],
+    ['italic', 8, 5],
+    ['syntax', 13, 1],
+  ]);
+});
+
+test('strikethrough', () => {
+  expect('Hello, ~world~!').toBeParsedAs(SAME, [
+    ['syntax', 7, 1],
+    ['strikethrough', 8, 5],
+    ['syntax', 13, 1],
+  ]);
+});
+
+describe('mention', () => {
+  test('normal', () => {
+    expect('@here Hello!').toBeParsedAs(SAME, [['mention', 0, 5]]);
+  });
+
+  test('with additional letters', () => {
+    expect('@herex').toBeParsedAs(SAME, []);
+  });
+
+  test('with punctation marks', () => {
+    expect('@here!').toBeParsedAs(SAME, [['mention', 0, 5]]);
+  });
+});
+
+describe('mention-user', () => {
+  test('normal', () => {
+    expect('@mail@mail.com Hello!').toBeParsedAs(SAME, [
+      ['mention-user', 0, 14],
+    ]);
+  });
+
+  test('without top-level domain', () => {
+    expect('@mail@mail').toBeParsedAs(SAME, []);
+  });
+
+  test('with punctation marks', () => {
+    expect('@mail@mail.com!').toBeParsedAs(SAME, [['mention-user', 0, 14]]);
+  });
+});
+
+test('plain link', () => {
+  expect('https://example.com').toBeParsedAs(SAME, [['link', 0, 19]]);
+});
+
+test('labeled link', () => {
+  expect('[Link](https://example.com)').toBeParsedAs(SAME, [
+    ['syntax', 0, 1],
+    ['syntax', 5, 2],
+    ['link', 7, 19],
+    ['syntax', 26, 1],
+  ]);
+});
+
+test('link with same label as href', () => {
+  expect('[https://example.com](https://example.com)').toBeParsedAs(
+    'https://example.com',
+    [['link', 0, 19]]
+  );
+});
+
+test('link with query string', () => {
+  expect('https://example.com?name=John&age=25&city=NewYork').toBeParsedAs(
+    SAME,
+    [['link', 0, 49]]
+  );
+});
+
+test('plain email', () => {
+  expect('someone@example.com').toBeParsedAs(SAME, [['link', 0, 19]]);
+});
+
+test('labeled email', () => {
+  expect('[Email](mailto:someone@example.com)').toBeParsedAs(SAME, [
+    ['syntax', 0, 1],
+    ['syntax', 6, 2],
+    ['link', 8, 26],
+    ['syntax', 34, 1],
+  ]);
+});
+
+describe('email with same label as address', () => {
+  test('label and address without "mailto:"', () => {
+    expect('[someone@example.com](someone@example.com)').toBeParsedAs(
+      'someone@example.com',
+      [['link', 0, 19]]
+    );
+  });
+
+  test('label with "mailto:"', () => {
+    expect('[mailto:someone@example.com](someone@example.com)').toBeParsedAs(
+      'someone@example.com',
+      [['link', 0, 19]]
+    );
+  });
+
+  test('address with "mailto:"', () => {
+    expect('[someone@example.com](mailto:someone@example.com)').toBeParsedAs(
+      'someone@example.com',
+      [['link', 0, 19]]
+    );
+  });
+
+  test('label and address with "mailto:"', () => {
+    expect(
+      '[mailto:someone@example.com](mailto:someone@example.com)'
+    ).toBeParsedAs('someone@example.com', [['link', 0, 19]]);
+  });
+});
+
+test('inline code', () => {
+  expect('Hello `world`!').toBeParsedAs(SAME, [
+    ['syntax', 6, 1],
+    ['code', 7, 5],
+    ['syntax', 12, 1],
+  ]);
+});
+
+test('codeblock', () => {
+  expect('```\nHello world!\n```').toBeParsedAs(SAME, [
+    ['syntax', 0, 3],
+    ['pre', 4, 13],
+    ['syntax', 17, 3],
+  ]);
+});
+
+describe('quote', () => {
+  test('with single space', () => {
+    expect('> Hello world!').toBeParsedAs(SAME, [
+      ['syntax', 0, 1],
+      ['blockquote', 0, 14],
+    ]);
+  });
+
+  test('with multiple spaces', () => {
+    expect('>      Hello world!').toBeParsedAs(SAME, [
+      ['syntax', 0, 1],
+      ['blockquote', 0, 19],
+    ]);
+  });
+
+  test('without space', () => {
+    expect('>Hello world!').toBeParsedAs(SAME, [
+      ['syntax', 0, 1],
+      ['blockquote', 0, 13],
+    ]);
+  });
+});
+
+test('multiple blockquotes', () => {
+  expect('> Hello\n> beautiful\n> world').toBeParsedAs(SAME, [
+    ['syntax', 0, 1],
+    ['blockquote', 0, 7],
+    ['syntax', 8, 1],
+    ['blockquote', 8, 11],
+    ['syntax', 20, 1],
+    ['blockquote', 20, 7],
+  ]);
+});
+
+test('separate blockquotes', () => {
+  expect('> Lorem ipsum\ndolor\n> sit amet').toBeParsedAs(SAME, [
+    ['syntax', 0, 1],
+    ['blockquote', 0, 13],
+    ['syntax', 20, 1],
+    ['blockquote', 20, 10],
+  ]);
+});
+
+test('heading', () => {
+  expect('# Hello world').toBeParsedAs(SAME, [
+    ['syntax', 0, 2],
+    ['h1', 2, 11],
+  ]);
+});
+
+test('nested bold and italic', () => {
+  expect('*_Hello_*, _*world*_!').toBeParsedAs(SAME, [
+    ['syntax', 0, 1],
+    ['syntax', 1, 1],
+    ['bold', 1, 7],
+    ['italic', 2, 5],
+    ['syntax', 7, 1],
+    ['syntax', 8, 1],
+    ['syntax', 11, 1],
+    ['syntax', 12, 1],
+    ['italic', 12, 7],
+    ['bold', 13, 5],
+    ['syntax', 18, 1],
+    ['syntax', 19, 1],
+  ]);
+});
+
+describe('nested heading in blockquote', () => {
+  test('without spaces', () => {
+    expect('># Hello world').toBeParsedAs(SAME, [
+      ['syntax', 0, 1],
+      ['blockquote', 0, 14],
+      ['syntax', 1, 2],
+      ['h1', 3, 11],
+    ]);
+  });
+
+  test('with single space', () => {
+    expect('> # Hello world').toBeParsedAs(SAME, [
+      ['syntax', 0, 1],
+      ['blockquote', 0, 15],
+      ['syntax', 2, 2],
+      ['h1', 4, 11],
+    ]);
+  });
+
+  test('with multiple spaces after #', () => {
+    expect('>#    Hello world').toBeParsedAs(SAME, [
+      ['syntax', 0, 1],
+      ['blockquote', 0, 17],
+      ['syntax', 1, 2],
+      ['h1', 3, 14],
+    ]);
+  });
+});
+
+describe('trailing whitespace', () => {
+  describe('after blockquote', () => {
+    test('nothing', () => {
+      expect('> Hello world').toBeParsedAs(SAME, [
+        ['syntax', 0, 1],
+        ['blockquote', 0, 13],
+      ]);
+    });
+
+    test('single space', () => {
+      expect('> Hello world ').toBeParsedAs(SAME, [
+        ['syntax', 0, 1],
+        ['blockquote', 0, 14],
+      ]);
+    });
+
+    test('newline', () => {
+      expect('> Hello world\n').toBeParsedAs(SAME, [
+        ['syntax', 0, 1],
+        ['blockquote', 0, 13],
+      ]);
+    });
+  });
+
+  describe('after heading', () => {
+    test('nothing', () => {
+      expect('# Hello world').toBeParsedAs(SAME, [
+        ['syntax', 0, 2],
+        ['h1', 2, 11],
+      ]);
+    });
+
+    test('single space', () => {
+      expect('# Hello world ').toBeParsedAs(SAME, [
+        ['syntax', 0, 2],
+        ['h1', 2, 12],
+      ]);
+    });
+
+    test('multiple spaces', () => {
+      expect('#   Hello world ').toBeParsedAs(SAME, [
+        ['syntax', 0, 2],
+        ['h1', 2, 14],
+      ]);
+    });
+
+    test('newline', () => {
+      expect('# Hello world\n').toBeParsedAs(SAME, [
+        ['syntax', 0, 2],
+        ['h1', 2, 11],
+      ]);
+    });
+
+    test('multiple quotes', () => {
+      expect('> # Hello\n> # world').toBeParsedAs(SAME, [
+        ['syntax', 0, 1],
+        ['blockquote', 0, 9],
+        ['syntax', 2, 2],
+        ['h1', 4, 5],
+        ['syntax', 10, 1],
+        ['blockquote', 10, 9],
+        ['syntax', 12, 2],
+        ['h1', 14, 5],
+      ]);
+    });
+  });
+});
---
+++ parser/index.js
@@ -0,0 +1,164 @@
+import { ExpensiMark } from 'expensify-common/lib/ExpensiMark';
+import _ from 'underscore';
+
+function parseMarkdownToHTML(markdown) {
+  const parser = ExpensiMark;
+  const html = parser.replace(markdown, {
+    shouldKeepWhitespace: true,
+  });
+  return html;
+}
+
+function parseHTMLToTokens(html) {
+  const tokens = [];
+  let left = 0;
+  while (true) {
+    const open = html.indexOf('<', left);
+    if (open === -1) {
+      if (left < html.length) {
+        tokens.push(['TEXT', html.substring(left)]);
+      }
+      break;
+    }
+    if (open !== left) {
+      tokens.push(['TEXT', html.substring(left, open)]);
+    }
+    const close = html.indexOf('>', open);
+    if (close === -1) {
+      throw new Error('Invalid HTML: no matching ">"');
+    }
+    tokens.push(['HTML', html.substring(open, close + 1)]);
+    left = close + 1;
+  }
+  return tokens;
+}
+
+function parseTokensToTree(tokens) {
+  const stack = [{ tag: '<>', children: [] }];
+  tokens.forEach(([type, payload]) => {
+    if (type === 'TEXT') {
+      const text = _.unescape(payload);
+      const top = stack[stack.length - 1];
+      top.children.push(text);
+    } else if (type === 'HTML') {
+      if (payload.startsWith('</')) {
+        // closing tag
+        const child = stack.pop();
+        const top = stack[stack.length - 1];
+        top.children.push(child);
+      } else {
+        // opening tag
+        stack.push({ tag: payload, children: [] });
+      }
+    } else {
+      throw new Error(`Unknown token type: ${type}`);
+    }
+  });
+  if (stack.length !== 1) {
+    throw new Error('Invalid HTML: unclosed tags');
+  }
+  return stack[0];
+}
+
+function parseTreeToTextAndRanges(tree) {
+  let text = '';
+
+  function processChildren(node) {
+    if (typeof node === 'string') {
+      text += node;
+    } else {
+      node.children.forEach(dfs);
+    }
+  }
+
+  function appendSyntax(syntax) {
+    addChildrenWithStyle(syntax, 'syntax');
+  }
+
+  function addChildrenWithStyle(node, style) {
+    const start = text.length;
+    processChildren(node);
+    const end = text.length;
+    ranges.push([style, start, end - start]);
+  }
+
+  const ranges = [];
+  function dfs(node) {
+    if (typeof node === 'string') {
+      text += node;
+    } else {
+      if (node.tag === '<>') {
+        processChildren(node);
+      } else if (node.tag === '<strong>') {
+        appendSyntax('*', 'syntax');
+        addChildrenWithStyle(node, 'bold');
+        appendSyntax('*');
+      } else if (node.tag === '<em>') {
+        appendSyntax('_');
+        addChildrenWithStyle(node, 'italic');
+        appendSyntax('_');
+      } else if (node.tag === '<del>') {
+        appendSyntax('~');
+        addChildrenWithStyle(node, 'strikethrough');
+        appendSyntax('~');
+      } else if (node.tag === '<code>') {
+        appendSyntax('`');
+        addChildrenWithStyle(node, 'code');
+        appendSyntax('`');
+      } else if (node.tag === '<mention-here>') {
+        addChildrenWithStyle(node, 'mention');
+      } else if (node.tag === '<mention-user>') {
+        addChildrenWithStyle(node, 'mention-user');
+      } else if (node.tag === '<blockquote>') {
+        appendSyntax('>');
+        addChildrenWithStyle(node, 'blockquote');
+        // compensate for "> " at the beginning
+        ranges[ranges.length - 1][1] -= 1;
+        ranges[ranges.length - 1][2] += 1;
+      } else if (node.tag === '<h1>') {
+        appendSyntax('# ');
+        addChildrenWithStyle(node, 'h1');
+      } else if (node.tag === '<pre>') {
+        appendSyntax('```');
+        text += '\n';
+        if (!node.children.every((child) => typeof child === 'string')) {
+          throw new Error('Invalid HTML: <pre> must contain only text');
+        }
+        const content = node.children.join('').replaceAll('&#32;', ' ');
+        addChildrenWithStyle(content, 'pre');
+        appendSyntax('```');
+      } else if (node.tag.startsWith('<a href="')) {
+        const href = _.unescape(node.tag.match(/href="([^"]*)"/)[1]);
+        if (
+          node.children.length === 1 &&
+          typeof node.children[0] === 'string' &&
+          (node.children[0] === href || `mailto:${node.children[0]}` === href)
+        ) {
+          addChildrenWithStyle(node.children[0], 'link');
+        } else {
+          appendSyntax('[');
+          processChildren(node);
+          appendSyntax('](');
+          addChildrenWithStyle(href, 'link');
+          appendSyntax(')');
+        }
+      } else {
+        throw new Error('Unknown tag: ' + node.tag);
+      }
+    }
+  }
+  dfs(tree);
+  return [text, ranges];
+}
+
+function parseMarkdownToTextAndRanges(markdown) {
+  const html = parseMarkdownToHTML(markdown);
+  const tokens = parseHTMLToTokens(html);
+  const tree = parseTokensToTree(tokens);
+  const [text, ranges] = parseTreeToTextAndRanges(tree);
+  ranges.sort((a, b) => a[1] - b[1]); // sort by location to properly handle bold+italic
+  return [text, ranges];
+}
+
+// eslint-disable-next-line no-undef
+globalThis.parseMarkdownToTextAndRanges = parseMarkdownToTextAndRanges;
