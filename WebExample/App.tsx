acf0efc522ffd7b4ae9750bba785c909e5a4d89e
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-12-12 11:58:02 +0100

    Setup Babel and Webpack

--- WebExample/App.tsx
+++ WebExample/App.tsx
@@ -1,11 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+
+import {
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+import { MarkdownTextInput } from 'react-native-markdown-text-input';
+
+function getPlatform() {
+  return Platform.select({
+    android: 'Android',
+    ios: 'iOS',
+    default: Platform.OS,
+  });
+}
+
+function getArchitecture() {
+  return 'nativeFabricUIManager' in global ? 'Fabric' : 'Paper';
+}
 
 export default function App() {
+  const [value, setValue] = React.useState('Hello, *world*!');
+
+  // TODO: use MarkdownTextInput ref instead of TextInput ref
+  const ref = React.useRef<TextInput>(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{getPlatform()}</Text>
+      <Text>{getArchitecture()}</Text>
+      <Text>MarkdownTextInput singleline</Text>
+      <MarkdownTextInput
+        autoCapitalize="none"
+        value={value}
+        onChangeText={setValue}
+        style={styles.input}
+        ref={ref}
+      />
+      <Text>MarkdownTextInput multiline</Text>
+      <MarkdownTextInput
+        multiline
+        autoCapitalize="none"
+        value={value}
+        onChangeText={setValue}
+        style={styles.input}
+      />
+      <Text>TextInput singleline</Text>
+      <TextInput
+        autoCapitalize="none"
+        value={value}
+        onChangeText={setValue}
+        style={styles.input}
+      />
+      <Text>TextInput multiline</Text>
+      <TextInput
+        multiline
+        autoCapitalize="none"
+        value={value}
+        onChangeText={setValue}
+        style={styles.input}
+      />
+      <Text style={styles.text}>{JSON.stringify(value)}</Text>
+      <Button title="Focus" onPress={() => ref.current?.focus()} />
+      <Button title="Blur" onPress={() => ref.current?.blur()} />
+      <Button title="Reset" onPress={() => setValue('Hello, *world*!')} />
+      <Button title="Clear" onPress={() => setValue('')} />
     </View>
   );
 }
@@ -13,8 +76,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 60,
+  },
+  input: {
+    fontSize: 20,
+    width: 300,
+    padding: 5,
+    borderColor: 'gray',
+    borderWidth: 1,
+    textAlignVertical: 'top',
+  },
+  text: {
+    fontFamily: 'Courier New',
+    marginTop: 10,
+    height: 100,
   },
 });
--- WebExample/babel.config.js
+++ WebExample/babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+const path = require('path');
+const pak = require('../package.json');
+
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            'react-native': path.join(
+              __dirname,
+              'node_modules',
+              'react-native-web'
+            ),
+            [pak.name]: path.join(__dirname, '..', pak.source),
+          },
+        },
+      ],
+    ],
   };
 };
