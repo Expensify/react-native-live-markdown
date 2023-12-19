dd7301e00c4fef3feda82a62fff5c2c68517b402
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-12-19 09:13:46 +0100

    Revert separate change

--- ios/RCTUITextView+Markdown.mm
+++ ios/RCTUITextView+Markdown.mm
@@ -20,11 +20,7 @@
     UITextRange *range = self.selectedTextRange;
     super.attributedText = [RCTMarkdownUtils parseMarkdown:self.attributedText];
     [super setSelectedTextRange:range]; // prevents cursor from jumping at the end when typing in the middle of the text
-
-    if ([self.attributedText length] > 0) {
-      UIFont *font = [self.attributedText attribute:NSFontAttributeName atIndex:0 effectiveRange:NULL];
-      self.typingAttributes = @{NSFontAttributeName:font}; // removes indent in new line when typing after quote
-    }
+    self.typingAttributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:20]}; // removes indent in new line when typing after quote
   }
 
   // Call the original method
