8a76908b9a99b85e176ba4a1fe57a0ef1679c61f
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2023-12-19 02:16:38 +0100

    fix: remove comment

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,5 +3,5 @@ export {};
 type Range = [string, number, number];
 
 declare global {
-  var parseMarkdownToTextAndRanges: (markdown: string) => [string, Range[]]; // Replace any with your specific return type
+  var parseMarkdownToTextAndRanges: (markdown: string) => [string, Range[]];
 }
