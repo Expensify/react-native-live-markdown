544446e74bac40d1ba6b138f99214afd6c06f506
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2023-12-22 22:27:14 +0100

    Remove color prop

--- android/src/main/java/com/markdowntextinput/MarkdownTextInputViewManager.java
+++ android/src/main/java/com/markdowntextinput/MarkdownTextInputViewManager.java
@@ -1,12 +1,7 @@
 package com.markdowntextinput;
 
-import android.graphics.Color;
-
-import androidx.annotation.Nullable;
-
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.annotations.ReactProp;
 
 @ReactModule(name = MarkdownTextInputViewManager.NAME)
 public class MarkdownTextInputViewManager extends MarkdownTextInputViewManagerSpec<MarkdownTextInputView> {
@@ -21,11 +16,5 @@ public class MarkdownTextInputViewManager extends MarkdownTextInputViewManagerSp
   @Override
   public MarkdownTextInputView createViewInstance(ThemedReactContext context) {
     return new MarkdownTextInputView(context);
-  }
-
-  @Override
-  @ReactProp(name = "color")
-  public void setColor(MarkdownTextInputView view, @Nullable String color) {
-    view.setBackgroundColor(Color.parseColor(color));
   }
 }
