555d8b419805c305ac1ff9b4839db1b16eb9bdf3
Robert Kozik <robert.kozik@swmansion.com>
2024-01-02 13:45:45 +0100

    Always copy existing traits into desired traits (#66)
    Co-authored-by: Tomek Zawadzki <tomekzawadzki98@gmail.com>

--- ios/RCTMarkdownUtils.mm
+++ ios/RCTMarkdownUtils.mm
@@ -77,6 +77,9 @@ static CGFloat headingFontSize = 25;
       desiredTraits = existingTraits | UIFontDescriptorTraitMonoSpace;
     } else if ([type isEqualToString:@"syntax"]) {
       desiredTraits = UIFontDescriptorTraitBold; // TODO: remove italic in nested bold+italic
+    } else {
+      // keep existing traits regardless of current item type
+      desiredTraits = existingTraits;
     }
 
     UIFontDescriptor *newFontDescriptor = [fontDescriptor fontDescriptorWithSymbolicTraits:desiredTraits];
