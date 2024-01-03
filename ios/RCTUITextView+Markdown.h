f6e9edffff0045434e6055ea7b478acea18dbf20
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-01-03 11:44:18 +0100

    Fix null argument warning (#84)

--- ios/RCTUITextView+Markdown.h
+++ ios/RCTUITextView+Markdown.h
@@ -6,7 +6,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RCTUITextView (Markdown)
 
-@property(nonatomic, getter=getMarkdownUtils) RCTMarkdownUtils *markdownUtils;
+@property(nonatomic, nullable, getter=getMarkdownUtils) RCTMarkdownUtils *markdownUtils;
 
 - (void)markdown_textDidChange;
 
