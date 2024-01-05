28718b4872626ebd797be093ec3342088ffb0b6a
Bartosz Grajdek <bartosz.grajdek@swmansion.com>
2024-01-05 10:58:42 +0100

    Change project configuration (#44)

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,5 +3,6 @@ export {};
 type Range = [string, number, number];
 
 declare global {
+  // eslint-disable-next-line no-var
   var parseMarkdownToTextAndRanges: (markdown: string) => [string, Range[]];
 }
