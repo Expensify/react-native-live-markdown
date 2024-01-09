11bd2e563a7443f560bb7fdbe0808ae416b9dcda
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-01-09 23:06:41 +0100

    Improve class naming on iOS (#110)

--- ios/MarkdownTextInputDecoratorView.mm
+++ ios/MarkdownTextInputDecoratorView.mm
@@ -1,61 +1,104 @@
+#import <React/RCTUITextField.h>
+#import <react/debug/react_native_assert.h>
+
+#import <react-native-live-markdown/MarkdownLayoutManager.h>
+#import <react-native-live-markdown/MarkdownTextInputDecoratorView.h>
+#import <react-native-live-markdown/RCTBackedTextFieldDelegateAdapter+Markdown.h>
+#import <react-native-live-markdown/RCTUITextView+Markdown.h>
+
 #ifdef RCT_NEW_ARCH_ENABLED
-#import "MarkdownTextInputDecoratorView.h"
+#import <react-native-live-markdown/RCTTextInputComponentView+Markdown.h>
+#else
+#import <react-native-live-markdown/RCTBaseTextInputView+Markdown.h>
+#endif /* RCT_NEW_ARCH_ENABLED */
 
-#import <react/renderer/components/RNLiveMarkdownSpec/ComponentDescriptors.h>
-#import <react/renderer/components/RNLiveMarkdownSpec/EventEmitters.h>
-#import <react/renderer/components/RNLiveMarkdownSpec/Props.h>
-#import <react/renderer/components/RNLiveMarkdownSpec/RCTComponentViewHelpers.h>
-
-#import <react-native-live-markdown/MarkdownTextInputDecoratorViewView.h>
-#import <react-native-live-markdown/RCTMarkdownStyle.h>
-
-#import "RCTFabricComponentsPlugins.h"
-
-using namespace facebook::react;
-
-@interface MarkdownTextInputDecoratorView () <RCTMarkdownTextInputDecoratorViewViewProtocol>
-
-@end
+#import <objc/runtime.h>
 
 @implementation MarkdownTextInputDecoratorView {
-  MarkdownTextInputDecoratorViewView *_view;
+  RCTMarkdownUtils *_markdownUtils;
+  RCTMarkdownStyle *_markdownStyle;
+#ifdef RCT_NEW_ARCH_ENABLED
+  __weak RCTTextInputComponentView *_textInput;
+#else
+  __weak RCTBaseTextInputView *_textInput;
+#endif /* RCT_NEW_ARCH_ENABLED */
+  __weak RCTBackedTextFieldDelegateAdapter *_adapter;
+  __weak RCTUITextView *_textView;
 }
 
-+ (ComponentDescriptorProvider)componentDescriptorProvider
-{
-  return concreteComponentDescriptorProvider<MarkdownTextInputDecoratorViewComponentDescriptor>();
-}
-
-- (instancetype)initWithFrame:(CGRect)frame
-{
-  if (self = [super initWithFrame:frame]) {
-    static const auto defaultProps = std::make_shared<const MarkdownTextInputDecoratorViewProps>();
-    _props = defaultProps;
-
-    _view = [[MarkdownTextInputDecoratorViewView alloc] init];
-
-    self.contentView = _view;
+- (void)didMoveToWindow {
+#ifdef RCT_NEW_ARCH_ENABLED
+  if (self.superview.superview == nil) {
+    return;
   }
+#else
+  if (self.superview == nil) {
+    return;
+  }
+#endif /* RCT_NEW_ARCH_ENABLED */
 
-  return self;
+#ifdef RCT_NEW_ARCH_ENABLED
+  NSArray *viewsArray = self.superview.superview.subviews;
+  NSUInteger currentIndex = [viewsArray indexOfObject:self.superview];
+#else
+  NSArray *viewsArray = self.superview.subviews;
+  NSUInteger currentIndex = [viewsArray indexOfObject:self];
+#endif /* RCT_NEW_ARCH_ENABLED */
+
+  react_native_assert(currentIndex != 0 && currentIndex != NSNotFound && "Error while finding current component.");
+  UIView *view = [viewsArray objectAtIndex:currentIndex - 1];
+
+#ifdef RCT_NEW_ARCH_ENABLED
+  react_native_assert([view isKindOfClass:[RCTTextInputComponentView class]] && "Previous sibling component is not an instance of RCTTextInputComponentView.");
+  _textInput = (RCTTextInputComponentView *)view;
+  UIView<RCTBackedTextInputViewProtocol> *backedTextInputView = [_textInput valueForKey:@"_backedTextInputView"];
+#else
+  react_native_assert([view isKindOfClass:[RCTBaseTextInputView class]] && "Previous sibling component is not an instance of RCTBaseTextInputView.");
+  _textInput = (RCTBaseTextInputView *)view;
+  UIView<RCTBackedTextInputViewProtocol> *backedTextInputView = _textInput.backedTextInputView;
+#endif /* RCT_NEW_ARCH_ENABLED */
+
+  _markdownUtils = [[RCTMarkdownUtils alloc] initWithBackedTextInputView:backedTextInputView];
+  react_native_assert(_markdownStyle != nil);
+  [_markdownUtils setMarkdownStyle:_markdownStyle];
+
+  [_textInput setMarkdownUtils:_markdownUtils];
+  if ([backedTextInputView isKindOfClass:[RCTUITextField class]]) {
+    RCTUITextField *textField = (RCTUITextField *)backedTextInputView;
+    _adapter = [textField valueForKey:@"textInputDelegateAdapter"];
+    [_adapter setMarkdownUtils:_markdownUtils];
+  } else if ([backedTextInputView isKindOfClass:[RCTUITextView class]]) {
+    _textView = (RCTUITextView *)backedTextInputView;
+    [_textView setMarkdownUtils:_markdownUtils];
+    object_setClass(_textView.layoutManager, [MarkdownLayoutManager class]);
+    [_textView.layoutManager setValue:_markdownUtils forKey:@"markdownUtils"];
+  } else {
+    react_native_assert(false && "Cannot enable Markdown for this type of TextInput.");
+  }
 }
 
-- (void)updateProps:(Props::Shared const &)props oldProps:(Props::Shared const &)oldProps
+- (void)willMoveToWindow:(UIWindow *)newWindow
 {
-    const auto &oldViewProps = *std::static_pointer_cast<MarkdownTextInputDecoratorViewProps const>(_props);
-    const auto &newViewProps = *std::static_pointer_cast<MarkdownTextInputDecoratorViewProps const>(props);
-
-    // TODO: if (oldViewProps.markdownStyle != newViewProps.markdownStyle)
-    RCTMarkdownStyle *markdownStyle = [[RCTMarkdownStyle alloc] initWithStruct:newViewProps.markdownStyle];
-    [_view setMarkdownStyle:markdownStyle];
-
-    [super updateProps:props oldProps:oldProps];
+  if (_textInput != nil) {
+    [_textInput setMarkdownUtils:nil];
+  }
+  if (_adapter != nil) {
+    [_adapter setMarkdownUtils:nil];
+  }
+  if (_textView != nil) {
+    [_textView setMarkdownUtils:nil];
+    if (_textView.layoutManager != nil && [object_getClass(_textView.layoutManager) isEqual:[MarkdownLayoutManager class]]) {
+      [_textView.layoutManager setValue:nil forKey:@"markdownUtils"];
+      object_setClass(_textView.layoutManager, [NSLayoutManager class]);
+    }
+  }
 }
 
-Class<RCTComponentViewProtocol> MarkdownTextInputDecoratorViewCls(void)
+- (void)setMarkdownStyle:(nonnull RCTMarkdownStyle *)markdownStyle
 {
-    return MarkdownTextInputDecoratorView.class;
+  _markdownStyle = markdownStyle;
+  [_markdownUtils setMarkdownStyle:markdownStyle];
+  [_textInput textInputDidChange]; // trigger attributed text update
 }
 
 @end
-#endif
