179b55ede4d0ec2ec978a75a04767138fe1f39d5
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-01-09 10:44:01 +0100

    Fix Webpack error

--- WebExample/webpack.config.js
+++ WebExample/webpack.config.js
@@ -1,12 +1,11 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
-const pak = require('../package.json');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(
     {
       ...env,
       babel: {
-        dangerouslyAddModulePathsToTranspile: [pak.name],
+        dangerouslyAddModulePathsToTranspile: ['react-native-live-markdown'],
       },
     },
     argv
