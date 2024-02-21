5f3d841d97792bd9f40992055ed6b0e979c11024
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-02-21 12:32:27 +0100

    Update js files

--- src/MarkdownTextInput.tsx
+++ src/MarkdownTextInput.tsx
@@ -2,9 +2,14 @@ import {StyleSheet, TextInput, processColor} from 'react-native';
 import React from 'react';
 import type {TextInputProps} from 'react-native';
 import MarkdownTextInputDecoratorViewNativeComponent from './MarkdownTextInputDecoratorViewNativeComponent';
+import NativeMarkdownModule from './NativeMarkdownModule';
 import type * as MarkdownTextInputDecoratorViewNativeComponentTypes from './MarkdownTextInputDecoratorViewNativeComponent';
 import * as StyleUtils from './styleUtils';
 import type * as StyleUtilsTypes from './styleUtils';
+
+if (NativeMarkdownModule) {
+  NativeMarkdownModule.install();
+}
 
 type PartialMarkdownStyle = StyleUtilsTypes.PartialMarkdownStyle;
 type MarkdownStyle = MarkdownTextInputDecoratorViewNativeComponentTypes.MarkdownStyle;
