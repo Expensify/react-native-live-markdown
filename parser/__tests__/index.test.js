0ad538ac57964775ac2c8fdcece2b043fb547430
Robert Kozik <robert.kozik@swmansion.com>
2024-02-21 12:12:14 +0100

    update parser tests

--- parser/__tests__/index.test.js
+++ parser/__tests__/index.test.js
@@ -23,31 +23,31 @@ test('no formatting', () => {
 
 test('bold', () => {
   expect('Hello, *world*!').toBeParsedAs([
-    ['syntax', 7, 1],
-    ['bold', 8, 5],
-    ['syntax', 13, 1],
+    {style: 'syntax', start: 7, length: 1},
+    {style: 'bold', start: 8, length: 5},
+    {style: 'syntax', start: 13, length: 1},
   ]);
 });
 
 test('italic', () => {
   expect('Hello, _world_!').toBeParsedAs([
-    ['syntax', 7, 1],
-    ['italic', 8, 5],
-    ['syntax', 13, 1],
+    {style: 'syntax', start: 7, length: 1},
+    {style: 'italic', start: 8, length: 5},
+    {style: 'syntax', start: 13, length: 1},
   ]);
 });
 
 test('strikethrough', () => {
   expect('Hello, ~world~!').toBeParsedAs([
-    ['syntax', 7, 1],
-    ['strikethrough', 8, 5],
-    ['syntax', 13, 1],
+    {style: 'syntax', start: 7, length: 1},
+    {style: 'strikethrough', start: 8, length: 5},
+    {style: 'syntax', start: 13, length: 1},
   ]);
 });
 
 describe('mention-here', () => {
   test('normal', () => {
-    expect('@here Hello!').toBeParsedAs([['mention-here', 0, 5]]);
+    expect('@here Hello!').toBeParsedAs([{style: 'mention-here', start: 0, length: 5}]);
   });
 
   test('with additional letters', () => {
@@ -55,13 +55,13 @@ describe('mention-here', () => {
   });
 
   test('with punctation marks', () => {
-    expect('@here!').toBeParsedAs([['mention-here', 0, 5]]);
+    expect('@here!').toBeParsedAs([{style: 'mention-here', start: 0, length: 5}]);
   });
 });
 
 describe('mention-user', () => {
   test('normal', () => {
-    expect('@mail@mail.com Hello!').toBeParsedAs([['mention-user', 0, 14]]);
+    expect('@mail@mail.com Hello!').toBeParsedAs([{style: 'mention-user', start: 0, length: 14}]);
   });
 
   test('without top-level domain', () => {
@@ -69,199 +69,199 @@ describe('mention-user', () => {
   });
 
   test('with punctation marks', () => {
-    expect('@mail@mail.com!').toBeParsedAs([['mention-user', 0, 14]]);
+    expect('@mail@mail.com!').toBeParsedAs([{style: 'mention-user', start: 0, length: 14}]);
   });
 });
 
 test('plain link', () => {
-  expect('https://example.com').toBeParsedAs([['link', 0, 19]]);
+  expect('https://example.com').toBeParsedAs([{style: 'link', start: 0, length: 19}]);
 });
 
 test('labeled link', () => {
   expect('[Link](https://example.com)').toBeParsedAs([
-    ['syntax', 0, 1],
-    ['syntax', 5, 2],
-    ['link', 7, 19],
-    ['syntax', 26, 1],
+    {style: 'syntax', start: 0, length: 1},
+    {style: 'syntax', start: 5, length: 2},
+    {style: 'link', start: 7, length: 19},
+    {style: 'syntax', start: 26, length: 1},
   ]);
 });
 
 test('link with same label as href', () => {
   expect('[https://example.com](https://example.com)').toBeParsedAs([
-    ['syntax', 0, 1],
-    ['syntax', 20, 2],
-    ['link', 22, 19],
-    ['syntax', 41, 1],
+    {style: 'syntax', start: 0, length: 1},
+    {style: 'syntax', start: 20, length: 2},
+    {style: 'link', start: 22, length: 19},
+    {style: 'syntax', start: 41, length: 1},
   ]);
 });
 
 test('no nesting links while typing', () => {
-  expect('[link](www.google.com').toBeParsedAs([['link', 7, 14]]);
+  expect('[link](www.google.com').toBeParsedAs([{style: 'link', start: 7, length: 14}]);
 });
 
 test('link with query string', () => {
-  expect('https://example.com?name=John&age=25&city=NewYork').toBeParsedAs([['link', 0, 49]]);
+  expect('https://example.com?name=John&age=25&city=NewYork').toBeParsedAs([{style: 'link', start: 0, length: 49}]);
 });
 
 test('plain email', () => {
-  expect('someone@example.com').toBeParsedAs([['link', 0, 19]]);
+  expect('someone@example.com').toBeParsedAs([{style: 'link', start: 0, length: 19}]);
 });
 
 test('labeled email', () => {
   expect('[Email](mailto:someone@example.com)').toBeParsedAs([
-    ['syntax', 0, 1],
-    ['syntax', 6, 2],
-    ['link', 8, 26],
-    ['syntax', 34, 1],
+    {style: 'syntax', start: 0, length: 1},
+    {style: 'syntax', start: 6, length: 2},
+    {style: 'link', start: 8, length: 26},
+    {style: 'syntax', start: 34, length: 1},
   ]);
 });
 
 describe('email with same label as address', () => {
   test('label and address without "mailto:"', () => {
     expect('[someone@example.com](someone@example.com)').toBeParsedAs([
-      ['syntax', 0, 1],
-      ['syntax', 20, 2],
-      ['link', 22, 19],
-      ['syntax', 41, 1],
+      {style: 'syntax', start: 0, length: 1},
+      {style: 'syntax', start: 20, length: 2},
+      {style: 'link', start: 22, length: 19},
+      {style: 'syntax', start: 41, length: 1},
     ]);
   });
 
   test('label with "mailto:"', () => {
     expect('[mailto:someone@example.com](someone@example.com)').toBeParsedAs([
-      ['syntax', 0, 1],
-      ['syntax', 27, 2],
-      ['link', 29, 19],
-      ['syntax', 48, 1],
+      {style: 'syntax', start: 0, length: 1},
+      {style: 'syntax', start: 27, length: 2},
+      {style: 'link', start: 29, length: 19},
+      {style: 'syntax', start: 48, length: 1},
     ]);
   });
 
   test('address with "mailto:"', () => {
     expect('[someone@example.com](mailto:someone@example.com)').toBeParsedAs([
-      ['syntax', 0, 1],
-      ['syntax', 20, 2],
-      ['link', 22, 26],
-      ['syntax', 48, 1],
+      {style: 'syntax', start: 0, length: 1},
+      {style: 'syntax', start: 20, length: 2},
+      {style: 'link', start: 22, length: 26},
+      {style: 'syntax', start: 48, length: 1},
     ]);
   });
 
   test('label and address with "mailto:"', () => {
     expect('[mailto:someone@example.com](mailto:someone@example.com)').toBeParsedAs([
-      ['syntax', 0, 1],
-      ['syntax', 27, 2],
-      ['link', 29, 26],
-      ['syntax', 55, 1],
+      {style: 'syntax', start: 0, length: 1},
+      {style: 'syntax', start: 27, length: 2},
+      {style: 'link', start: 29, length: 26},
+      {style: 'syntax', start: 55, length: 1},
     ]);
   });
 });
 
 test('inline code', () => {
   expect('Hello `world`!').toBeParsedAs([
-    ['syntax', 6, 1],
-    ['code', 7, 5],
-    ['syntax', 12, 1],
+    {style: 'syntax', start: 6, length: 1},
+    {style: 'code', start: 7, length: 5},
+    {style: 'syntax', start: 12, length: 1},
   ]);
 });
 
 test('codeblock', () => {
   expect('```\nHello world!\n```').toBeParsedAs([
-    ['syntax', 0, 3],
-    ['pre', 3, 14],
-    ['syntax', 17, 3],
+    {style: 'syntax', start: 0, length: 3},
+    {style: 'pre', start: 3, length: 14},
+    {style: 'syntax', start: 17, length: 3},
   ]);
 });
 
 describe('blockquote', () => {
   test('with single space', () => {
     expect('> Hello world!').toBeParsedAs([
-      ['blockquote', 0, 14],
-      ['syntax', 0, 1],
+      {style: 'blockquote', start: 0, length: 14},
+      {style: 'syntax', start: 0, length: 1},
     ]);
   });
 
   test('with multiple spaces', () => {
     expect('>      Hello world!').toBeParsedAs([
-      ['blockquote', 0, 19],
-      ['syntax', 0, 1],
+      {style: 'blockquote', start: 0, length: 19},
+      {style: 'syntax', start: 0, length: 1},
     ]);
   });
 
   test('without space', () => {
     expect('>Hello world!').toBeParsedAs([
-      ['blockquote', 0, 13],
-      ['syntax', 0, 1],
+      {style: 'blockquote', start: 0, length: 13},
+      {style: 'syntax', start: 0, length: 1},
     ]);
   });
 });
 
 test('multiple blockquotes', () => {
   expect('> Hello\n> beautiful\n> world').toBeParsedAs([
-    ['blockquote', 0, 7],
-    ['syntax', 0, 1],
-    ['blockquote', 8, 11],
-    ['syntax', 8, 1],
-    ['blockquote', 20, 7],
-    ['syntax', 20, 1],
+    {style: 'blockquote', start: 0, length: 7},
+    {style: 'syntax', start: 0, length: 1},
+    {style: 'blockquote', start: 8, length: 11},
+    {style: 'syntax', start: 8, length: 1},
+    {style: 'blockquote', start: 20, length: 7},
+    {style: 'syntax', start: 20, length: 1},
   ]);
 });
 
 test('separate blockquotes', () => {
   expect('> Lorem ipsum\ndolor\n> sit amet').toBeParsedAs([
-    ['blockquote', 0, 13],
-    ['syntax', 0, 1],
-    ['blockquote', 20, 10],
-    ['syntax', 20, 1],
+    {style: 'blockquote', start: 0, length: 13},
+    {style: 'syntax', start: 0, length: 1},
+    {style: 'blockquote', start: 20, length: 10},
+    {style: 'syntax', start: 20, length: 1},
   ]);
 });
 
 test('h1', () => {
   expect('# Hello world').toBeParsedAs([
-    ['syntax', 0, 2],
-    ['h1', 2, 11],
+    {style: 'syntax', start: 0, length: 2},
+    {style: 'h1', start: 2, length: 11},
   ]);
 });
 
 test('nested bold and italic', () => {
   expect('*_Hello_*, _*world*_!').toBeParsedAs([
-    ['syntax', 0, 1],
-    ['bold', 1, 7],
-    ['syntax', 1, 1],
-    ['italic', 2, 5],
-    ['syntax', 7, 1],
-    ['syntax', 8, 1],
-    ['syntax', 11, 1],
-    ['italic', 12, 7],
-    ['syntax', 12, 1],
-    ['bold', 13, 5],
-    ['syntax', 18, 1],
-    ['syntax', 19, 1],
+    {style: 'syntax', start: 0, length: 1},
+    {style: 'bold', start: 1, length: 7},
+    {style: 'syntax', start: 1, length: 1},
+    {style: 'italic', start: 2, length: 5},
+    {style: 'syntax', start: 7, length: 1},
+    {style: 'syntax', start: 8, length: 1},
+    {style: 'syntax', start: 11, length: 1},
+    {style: 'italic', start: 12, length: 7},
+    {style: 'syntax', start: 12, length: 1},
+    {style: 'bold', start: 13, length: 5},
+    {style: 'syntax', start: 18, length: 1},
+    {style: 'syntax', start: 19, length: 1},
   ]);
 });
 
 describe('nested h1 in blockquote', () => {
   test('without spaces', () => {
     expect('># Hello world').toBeParsedAs([
-      ['blockquote', 0, 14],
-      ['syntax', 0, 1],
-      ['syntax', 1, 2],
-      ['h1', 3, 11],
+      {style: 'blockquote', start: 0, length: 14},
+      {style: 'syntax', start: 0, length: 1},
+      {style: 'syntax', start: 1, length: 2},
+      {style: 'h1', start: 3, length: 11},
     ]);
   });
 
   test('with single space', () => {
     expect('> # Hello world').toBeParsedAs([
-      ['blockquote', 0, 15],
-      ['syntax', 0, 1],
-      ['syntax', 2, 2],
-      ['h1', 4, 11],
+      {style: 'blockquote', start: 0, length: 15},
+      {style: 'syntax', start: 0, length: 1},
+      {style: 'syntax', start: 2, length: 2},
+      {style: 'h1', start: 4, length: 11},
     ]);
   });
 
   test('with multiple spaces after #', () => {
     expect('>#    Hello world').toBeParsedAs([
-      ['blockquote', 0, 17],
-      ['syntax', 0, 1],
-      ['syntax', 1, 2],
-      ['h1', 3, 14],
+      {style: 'blockquote', start: 0, length: 17},
+      {style: 'syntax', start: 0, length: 1},
+      {style: 'syntax', start: 1, length: 2},
+      {style: 'h1', start: 3, length: 14},
     ]);
   });
 });
@@ -270,22 +270,22 @@ describe('trailing whitespace', () => {
   describe('after blockquote', () => {
     test('nothing', () => {
       expect('> Hello world').toBeParsedAs([
-        ['blockquote', 0, 13],
-        ['syntax', 0, 1],
+        {style: 'blockquote', start: 0, length: 13},
+        {style: 'syntax', start: 0, length: 1},
       ]);
     });
 
     test('single space', () => {
       expect('> Hello world ').toBeParsedAs([
-        ['blockquote', 0, 14],
-        ['syntax', 0, 1],
+        {style: 'blockquote', start: 0, length: 14},
+        {style: 'syntax', start: 0, length: 1},
       ]);
     });
 
     test('newline', () => {
       expect('> Hello world\n').toBeParsedAs([
-        ['blockquote', 0, 13],
-        ['syntax', 0, 1],
+        {style: 'blockquote', start: 0, length: 13},
+        {style: 'syntax', start: 0, length: 1},
       ]);
     });
   });
@@ -293,42 +293,42 @@ describe('trailing whitespace', () => {
   describe('after h1', () => {
     test('nothing', () => {
       expect('# Hello world').toBeParsedAs([
-        ['syntax', 0, 2],
-        ['h1', 2, 11],
+        {style: 'syntax', start: 0, length: 2},
+        {style: 'h1', start: 2, length: 11},
       ]);
     });
 
     test('single space', () => {
       expect('# Hello world ').toBeParsedAs([
-        ['syntax', 0, 2],
-        ['h1', 2, 12],
+        {style: 'syntax', start: 0, length: 2},
+        {style: 'h1', start: 2, length: 12},
       ]);
     });
 
     test('multiple spaces', () => {
       expect('#   Hello world ').toBeParsedAs([
-        ['syntax', 0, 2],
-        ['h1', 2, 14],
+        {style: 'syntax', start: 0, length: 2},
+        {style: 'h1', start: 2, length: 14},
       ]);
     });
 
     test('newline', () => {
       expect('# Hello world\n').toBeParsedAs([
-        ['syntax', 0, 2],
-        ['h1', 2, 11],
+        {style: 'syntax', start: 0, length: 2},
+        {style: 'h1', start: 2, length: 11},
       ]);
     });
 
     test('multiple blockquotes', () => {
       expect('> # Hello\n> # world').toBeParsedAs([
-        ['blockquote', 0, 9],
-        ['syntax', 0, 1],
-        ['syntax', 2, 2],
-        ['h1', 4, 5],
-        ['blockquote', 10, 9],
-        ['syntax', 10, 1],
-        ['syntax', 12, 2],
-        ['h1', 14, 5],
+        {style: 'blockquote', start: 0, length: 9},
+        {style: 'syntax', start: 0, length: 1},
+        {style: 'syntax', start: 2, length: 2},
+        {style: 'h1', start: 4, length: 5},
+        {style: 'blockquote', start: 10, length: 9},
+        {style: 'syntax', start: 10, length: 1},
+        {style: 'syntax', start: 12, length: 2},
+        {style: 'h1', start: 14, length: 5},
       ]);
     });
   });
