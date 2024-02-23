6acf6cc88611b216e77db6ea8d8a5aa50ffe58f4
Robert Kozik <robert.kozik@swmansion.com>
2024-02-23 11:53:16 +0100

    adjust web implementation to handle object as range type

--- src/web/parserUtils.ts
+++ src/web/parserUtils.ts
@@ -7,8 +7,9 @@ type MarkdownType = 'bold' | 'italic' | 'strikethrough' | 'link' | 'code' | 'pre
 
 type MarkdownRange = {
   type: MarkdownType;
-  startIndex: number;
+  start: number;
   length: number;
+  depth?: number;
 };
 
 type NestedNode = {
@@ -99,10 +100,10 @@ function parseRangesToHTMLNodes(text: string, ranges: MarkdownRange[], markdownS
       break;
     }
 
-    const endOfCurrentRange = range.startIndex + range.length;
-    const nextRangeStartIndex = stack.length > 0 && !!stack[0] ? stack[0].startIndex || 0 : textLength;
+    const endOfCurrentRange = range.start + range.length;
+    const nextRangeStartIndex = stack.length > 0 && !!stack[0] ? stack[0].start || 0 : textLength;
 
-    addSubstringAsTextNode(currentRoot.node, text, lastRangeEndIndex, range.startIndex); // add text with newlines before current range
+    addSubstringAsTextNode(currentRoot.node, text, lastRangeEndIndex, range.start); // add text with newlines before current range
 
     const span = document.createElement('span');
     if (disableInlineStyles) {
@@ -115,9 +116,9 @@ function parseRangesToHTMLNodes(text: string, ranges: MarkdownRange[], markdownS
       // tag nesting
       currentRoot.node.appendChild(span);
       nestedStack.push({node: span, endIndex: endOfCurrentRange});
-      lastRangeEndIndex = range.startIndex;
+      lastRangeEndIndex = range.start;
     } else {
-      addSubstringAsTextNode(span, text, range.startIndex, endOfCurrentRange);
+      addSubstringAsTextNode(span, text, range.start, endOfCurrentRange);
       currentRoot.node.appendChild(span);
       lastRangeEndIndex = endOfCurrentRange;
 
@@ -162,14 +163,7 @@ function parseText(
   }
   const ranges = global.parseExpensiMarkToRanges(text);
 
-  const markdownRanges: MarkdownRange[] = ranges.map((range) => {
-    const [type, startIndex, length] = range;
-    return {
-      type: type as MarkdownType,
-      startIndex,
-      length,
-    };
-  });
+  const markdownRanges: MarkdownRange[] = ranges as MarkdownRange[];
 
   targetElement.innerHTML = '';
   targetElement.innerText = '';
