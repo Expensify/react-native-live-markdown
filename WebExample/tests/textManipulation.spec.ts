12ac98d70477e3c651e1e66f118b693126ff8f56
BrtqKr <bartlomiej.krason@swmansion.com>
2024-04-18 17:21:47 +0200

    add paste replace and quick type

--- WebExample/tests/textManipulation.spec.ts
+++ WebExample/tests/textManipulation.spec.ts
@@ -2,9 +2,9 @@ import {test, expect} from '@playwright/test';
 import type {Locator, Page} from '@playwright/test';
 import * as CONSTANTS from '../../constants';
 
-const setupInput = async (page: Page) => {
+const setupInput = async (page: Page, mode: 'clear' | 'reset') => {
   const inputLocator = await page.locator(`div#MarkdownInput_Example`);
-  await page.click('[data-testid="clear"]');
+  await page.click(`[data-testid="${mode}"]`);
 
   return inputLocator;
 };
@@ -20,17 +20,6 @@ test.beforeEach(async ({page}) => {
   //   await page.click('[data-testid="clear"]');
 });
 
-// test('copy', async ({page}) => {
-//   const inputLocator = (await setupInput(page)).locator('span.root').last();
-
-//   if (inputLocator) {
-//     const innerHTML = await inputLocator.evaluate((el) => el.innerHTML);
-//     console.log(`InnerHTML of the element: ${innerHTML}`);
-//   }
-
-//   //   await copyText({inputLocator, text: 'COPY'});
-// });
-
 const pasteContent = async ({text, page, inputLocator}: {text: string; page: Page; inputLocator: Locator}) => {
   await page.evaluate(async (pasteText) => navigator.clipboard.writeText(pasteText), text);
   await inputLocator.focus();
@@ -42,7 +31,7 @@ test('paste', async ({page}) => {
   const PASTE_TEXT = 'bold';
   const boldStyleDefinition = CONSTANTS.MARKDOWN_STYLE_DEFINITIONS.bold;
 
-  const inputLocator = await setupInput(page);
+  const inputLocator = await setupInput(page, 'clear');
 
   const wrappedText = boldStyleDefinition.wrapContent(PASTE_TEXT);
   await pasteContent({text: wrappedText, page, inputLocator});
@@ -56,3 +45,67 @@ test('paste', async ({page}) => {
   }
   expect(elementStyle).toEqual(boldStyleDefinition.style);
 });
+
+test('select', async ({page}) => {
+  const inputLocator = await setupInput(page, 'reset');
+  //   await pasteContent({text: SELECTION_TEXT, page, inputLocator});
+  await inputLocator.focus();
+
+  const cursorPosition = await page.evaluate(() => {
+    const editableDiv = document.querySelector('div[contenteditable="true"]');
+    const range = window.getSelection()?.getRangeAt(0);
+    if (!range || !editableDiv) return null;
+    const preCaretRange = range.cloneRange();
+    preCaretRange.selectNodeContents(editableDiv);
+    preCaretRange.setEnd(range.endContainer, range.endOffset);
+    return preCaretRange.toString().length;
+  });
+
+  expect(cursorPosition).toBe(CONSTANTS.EXAMPLE_CONTENT.length);
+});
+
+test('paste replace', async ({page}) => {
+  const modifier = process.platform === 'darwin' ? 'Meta' : 'Control';
+
+  const inputLocator = await setupInput(page, 'reset');
+
+  await inputLocator.focus();
+  await page.keyboard.down(modifier);
+  await page.keyboard.press('a');
+  await page.keyboard.up(modifier);
+
+  const newText = '*bold*';
+  await pasteContent({text: newText, page, inputLocator});
+
+  await page.evaluate(() => {
+    const editableDiv = document.querySelector('div[contenteditable="true"]');
+    return editableDiv;
+  });
+
+  expect(await inputLocator.innerText()).toBe(newText);
+});
+
+// github.com/Expensify/App/issues/39360
+test('fast type cursor position', async ({page}) => {
+  const inputLocator = await setupInput(page, 'clear');
+
+  const EXAMPLE_LONG_CONTENT = CONSTANTS.EXAMPLE_CONTENT.repeat(3);
+  await inputLocator.pressSequentially(EXAMPLE_LONG_CONTENT);
+
+  expect(await inputLocator.innerText()).toBe(EXAMPLE_LONG_CONTENT);
+
+  const cursorPosition = await page.evaluate((selector) => {
+    const editableDiv = document.querySelector(selector) as HTMLElement;
+    const range = window.getSelection()?.getRangeAt(0);
+    if (!range || !editableDiv) return null;
+    const preCaretRange = range.cloneRange();
+    preCaretRange.selectNodeContents(editableDiv);
+    preCaretRange.setEnd(range.endContainer, range.endOffset);
+    return preCaretRange.toString().length;
+  }, 'div[contenteditable="true"]');
+
+  expect(cursorPosition).toBe(EXAMPLE_LONG_CONTENT.length);
+});
+
+// COPY
+// CLEAR
