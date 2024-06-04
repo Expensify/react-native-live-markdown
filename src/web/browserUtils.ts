4a20b30c06e2bbf3ff9e34d59165134c8236a72e
Jakub Szymczak <szymczak.jakub12@gmail.com>
2024-06-04 15:27:52 +0200

    make TextInput ignore composition on mWeb

--- src/web/browserUtils.ts
+++ src/web/browserUtils.ts
@@ -1,4 +1,5 @@
 const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
 const isChromium = 'chrome' in window;
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Silk|Opera Mini/i.test(navigator.userAgent);
 
-export {isFirefox, isChromium};
+export {isFirefox, isChromium, isMobile};
