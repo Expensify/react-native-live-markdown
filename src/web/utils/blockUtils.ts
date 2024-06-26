2f829cc7b1ca8504801e46ff8a46febd0b58e27d
Michał Skałka <michal.skalka@swmansion.com>
2024-06-26 17:14:27 +0200

    Add block utils

---
+++ src/web/utils/blockUtils.ts
@@ -0,0 +1,65 @@
+import type {MarkdownType} from './parserUtils';
+import type {PartialMarkdownStyle} from '../../styleUtils';
+
+function addBlockStyling(targetElement: HTMLElement, type: MarkdownType, markdownStyle: PartialMarkdownStyle) {
+  const node = targetElement;
+  switch (type) {
+    case 'syntax':
+      Object.assign(node.style, markdownStyle.syntax);
+      break;
+    case 'bold':
+      node.style.fontWeight = 'bold';
+      break;
+    case 'italic':
+      node.style.fontStyle = 'italic';
+      break;
+    case 'strikethrough':
+      node.style.textDecoration = 'line-through';
+      break;
+    case 'emoji':
+      Object.assign(node.style, {...markdownStyle.emoji, verticalAlign: 'middle'});
+      break;
+    case 'mention-here':
+      Object.assign(node.style, markdownStyle.mentionHere);
+      break;
+    case 'mention-user':
+      Object.assign(node.style, markdownStyle.mentionUser);
+      break;
+    case 'mention-report':
+      Object.assign(node.style, markdownStyle.mentionReport);
+      break;
+    case 'link':
+      Object.assign(node.style, {
+        ...markdownStyle.link,
+        textDecoration: 'underline',
+      });
+      break;
+    case 'code':
+      Object.assign(node.style, markdownStyle.code);
+      break;
+    case 'pre':
+      Object.assign(node.style, markdownStyle.pre);
+      break;
+
+    case 'blockquote':
+      Object.assign(node.style, {
+        ...markdownStyle.blockquote,
+        borderLeftStyle: 'solid',
+        display: 'inline-block',
+        maxWidth: '100%',
+        boxSizing: 'border-box',
+      });
+      break;
+    case 'h1':
+      Object.assign(node.style, {
+        ...markdownStyle.h1,
+        fontWeight: 'bold',
+      });
+      break;
+    default:
+      break;
+  }
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export {addBlockStyling};
--- src/web/utils/parserUtils.ts
+++ src/web/utils/parserUtils.ts
@@ -4,6 +4,7 @@ import {addNodeToTree, buildTree} from './treeUtils';
 import type {NodeType, TreeNode} from './treeUtils';
 import type {PartialMarkdownStyle} from '../../styleUtils';
 import {getCurrentCursorPosition, moveCursorToEnd, setCursorPosition} from './cursorUtils';
+import {addBlockStyling} from './blockUtils';
 
 type MarkdownType = 'bold' | 'italic' | 'strikethrough' | 'emoji' | 'link' | 'code' | 'pre' | 'blockquote' | 'h1' | 'syntax' | 'mention-here' | 'mention-user' | 'mention-report';
 
@@ -20,66 +21,6 @@ type Paragraph = {
   length: number;
   markdownRanges: MarkdownRange[];
 };
-
-function addStyling(targetElement: HTMLElement, type: MarkdownType, markdownStyle: PartialMarkdownStyle) {
-  const node = targetElement;
-  switch (type) {
-    case 'syntax':
-      Object.assign(node.style, markdownStyle.syntax);
-      break;
-    case 'bold':
-      node.style.fontWeight = 'bold';
-      break;
-    case 'italic':
-      node.style.fontStyle = 'italic';
-      break;
-    case 'strikethrough':
-      node.style.textDecoration = 'line-through';
-      break;
-    case 'emoji':
-      Object.assign(node.style, {...markdownStyle.emoji, verticalAlign: 'middle'});
-      break;
-    case 'mention-here':
-      Object.assign(node.style, markdownStyle.mentionHere);
-      break;
-    case 'mention-user':
-      Object.assign(node.style, markdownStyle.mentionUser);
-      break;
-    case 'mention-report':
-      Object.assign(node.style, markdownStyle.mentionReport);
-      break;
-    case 'link':
-      Object.assign(node.style, {
-        ...markdownStyle.link,
-        textDecoration: 'underline',
-      });
-      break;
-    case 'code':
-      Object.assign(node.style, markdownStyle.code);
-      break;
-    case 'pre':
-      Object.assign(node.style, markdownStyle.pre);
-      break;
-
-    case 'blockquote':
-      Object.assign(node.style, {
-        ...markdownStyle.blockquote,
-        borderLeftStyle: 'solid',
-        display: 'inline-block',
-        maxWidth: '100%',
-        boxSizing: 'border-box',
-      });
-      break;
-    case 'h1':
-      Object.assign(node.style, {
-        ...markdownStyle.h1,
-        fontWeight: 'bold',
-      });
-      break;
-    default:
-      break;
-  }
-}
 
 function ungroupRanges(ranges: MarkdownRange[]): MarkdownRange[] {
   const ungroupedRanges: MarkdownRange[] = [];
@@ -267,7 +208,7 @@ function parseRangesToHTMLNodes(text: string, ranges: MarkdownRange[], markdownS
       if (disableInlineStyles) {
         span.className = range.type;
       } else {
-        addStyling(span, range.type, markdownStyle);
+        addBlockStyling(span, range.type, markdownStyle);
         span.setAttribute('data-type', range.type);
       }
 
