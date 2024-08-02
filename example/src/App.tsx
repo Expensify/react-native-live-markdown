1e394b3bd88efc1608084e471da4a12db70744d6
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-08-02 11:07:38 +0200

    Add expensify-common as peerDependency

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 
 import {Button, Platform, StyleSheet, Text, View} from 'react-native';
 
-import {MarkdownTextInput} from '@expensify/react-native-live-markdown';
+import {MarkdownTextInput, parseExpensiMark} from '@expensify/react-native-live-markdown';
 import type {TextInput} from 'react-native';
-import parseExpensiMark from './parseExpensiMark';
 
 function isWeb() {
   return Platform.OS === 'web';
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
 export {default as MarkdownTextInput} from './MarkdownTextInput';
 export type {MarkdownTextInputProps, MarkdownStyle, Range} from './MarkdownTextInput';
+export {default as parseExpensiMark} from './parseExpensiMark';
