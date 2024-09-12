748b93c6a5cdfef8aa52b2530fc21f06386f93b4
VickyStash <vikstash@gmail.com>
2024-09-12 11:39:49 +0200

    Add missing indentation

--- ios/MarkdownTextInputDecoratorView.mm
+++ ios/MarkdownTextInputDecoratorView.mm
@@ -111,14 +111,14 @@
   [_markdownUtils setMarkdownStyle:markdownStyle];
 
   if (_textView != nil) {
-      // We want to use `textStorage` for applying markdown when possible. Currently it's only available for UITextView
-      [_textView textDidChange];
+    // We want to use `textStorage` for applying markdown when possible. Currently it's only available for UITextView
+    [_textView textDidChange];
   } else {
-  // apply new styles
+    // apply new styles
 #ifdef RCT_NEW_ARCH_ENABLED
-  [_textInput _setAttributedString:_backedTextInputView.attributedText];
+    [_textInput _setAttributedString:_backedTextInputView.attributedText];
 #else
-  [_textInput setAttributedText:_textInput.attributedText];
+    [_textInput setAttributedText:_textInput.attributedText];
 #endif /* RCT_NEW_ARCH_ENABLED */
   }
 }
