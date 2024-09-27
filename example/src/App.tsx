2c0d0e01b57671538e268c7bea5b8cb26f5a4b2b
dominictb <tb-dominic@outlook.com>
2024-09-27 14:53:50 +0700

    fix: revert the example/App.tsx change

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -49,7 +49,7 @@ export default function App() {
         onSelectionChange={e => setSelection(e.nativeEvent.selection)}
         selection={selection}
         id={TEST_CONST.INPUT_ID}
-        maxLength={100}
+        maxLength={30000}
       />
       <Text style={styles.text}>{JSON.stringify(value)}</Text>
       <Button
