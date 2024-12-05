224a60abf46cd328f3817a669e9d9621da2e8a32
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-12-05 11:39:32 +0100

    RN 0.77

--- example/metro.config.js
+++ example/metro.config.js
@@ -11,7 +11,7 @@ const modules = Object.keys({...pak.peerDependencies});
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
   watchFolders: [root],
