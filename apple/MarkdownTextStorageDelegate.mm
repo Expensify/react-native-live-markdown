b5c0e119f0a5182d2be91f3909046d71b76c9a41
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-12-09 12:47:44 +0100

    Remove outdated TODO

--- apple/MarkdownTextStorageDelegate.mm
+++ apple/MarkdownTextStorageDelegate.mm
@@ -23,7 +23,6 @@
 
   [_markdownUtils applyMarkdownFormatting:textStorage withDefaultTextAttributes:_textView.defaultTextAttributes];
 
-  // TODO: fix cursor position when adding newline after a blockquote (probably not here though)
   // TODO: fix spellcheck not working for any of previous words when component value is controlled and contains bold (probably not here though)
 }
 
