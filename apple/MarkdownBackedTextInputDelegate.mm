4c70fba1462c7e897756e4e9671a7ccebca4fa59
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-12-12 16:58:05 +0100

    Fix regression with spellcheck disappearing immediately

--- apple/MarkdownBackedTextInputDelegate.mm
+++ apple/MarkdownBackedTextInputDelegate.mm
@@ -27,14 +27,8 @@
   // still contain NSParagraphStyle with non-zero firstLineHeadIndent and headIntent.
   // This causes the cursor to be shifted to the right instead of being located at the beginning of the line.
   // Also, if the previous line of the text ends with a link, there will be underline blinks visible while typing.
-  // The following code resets default text attributes to fix both problems at once.
-  // It seems like the setter performs deep comparision, so we differentiate the new value using a counter,
-  // otherwise this trick would work only once.
-  static NSAttributedStringKey RCTLiveMarkdownForceUpdateAttributeName = @"RCTLiveMarkdownForceUpdate";
-  static NSUInteger counter = 0;
-  NSMutableDictionary *defaultTextAttributes = [_textView.defaultTextAttributes mutableCopy];
-  defaultTextAttributes[RCTLiveMarkdownForceUpdateAttributeName] = @(counter++);
-  _textView.defaultTextAttributes = defaultTextAttributes;
+  // The following code resets typing attributes with default text attributes to fix both problems at once.
+  _textView.typingAttributes = _textView.defaultTextAttributes;
 
   // Delegate the call to the original text input delegate
   [_originalTextInputDelegate textInputDidChange];
