8dbd1bcdd256acf5f159a3f040c9da1298838fb7
Michał Skałka <michal.skalka@swmansion.com>
2024-12-19 09:43:51 +0100

    Add try/finallt block

--- android/src/main/java/com/expensify/livemarkdown/RangeSplitter.java
+++ android/src/main/java/com/expensify/livemarkdown/RangeSplitter.java
@@ -9,51 +9,56 @@ import java.util.List;
 
 public class RangeSplitter {
   public static ArrayList<MarkdownRange> splitRangesOnEmojis(@NonNull List<MarkdownRange> markdownRanges, @NonNull String type) {
-    Systrace.beginSection(0, "splitRangesOnEmojis");
     ArrayList<MarkdownRange> emojiRanges = new ArrayList<>();
     ArrayList<MarkdownRange> oldRanges = new ArrayList<>(markdownRanges);
     ArrayList<MarkdownRange> newRanges = new ArrayList<>();
-    for (MarkdownRange range : oldRanges) {
-      if (range.getType().equals("emoji")) {
-        emojiRanges.add(range);
-      }
-    }
 
-    int i = 0;
-    int j = 0;
-    while (i < oldRanges.size()) {
-      MarkdownRange currentRange = oldRanges.get(i);
-      if (!currentRange.getType().equals(type)) {
+    try {
+      Systrace.beginSection(0, "splitRangesOnEmojis");
+      for (MarkdownRange range : oldRanges) {
+        if (range.getType().equals("emoji")) {
+          emojiRanges.add(range);
+        }
+      }
+
+      int i = 0;
+      int j = 0;
+      while (i < oldRanges.size()) {
+        MarkdownRange currentRange = oldRanges.get(i);
+        if (!currentRange.getType().equals(type)) {
+          newRanges.add(currentRange);
+          i += 1;
+          continue;
+        }
+
+        // Iterate through all emoji ranges before the end of the current range, splitting the current range at each intersection.
+        while (j < emojiRanges.size()) {
+          MarkdownRange emojiRange = emojiRanges.get(j);
+          if (emojiRange.getStart() > currentRange.getEnd()) {
+            break;
+          }
+
+          int currentStart = currentRange.getStart();
+          int currentEnd = currentRange.getEnd();
+          int emojiStart = emojiRange.getStart();
+          int emojiEnd = emojiRange.getEnd();
+          if (emojiStart >= currentStart && emojiEnd <= currentEnd) { // Intersection
+            MarkdownRange newRange = new MarkdownRange(currentRange.getType(), currentStart, emojiStart - currentStart, currentRange.getDepth());
+            currentRange = new MarkdownRange(currentRange.getType(), emojiEnd, currentEnd - emojiEnd, currentRange.getDepth());
+
+            if (newRange.getLength() > 0) {
+              newRanges.add(newRange);
+            }
+          }
+          j += 1;
+        }
         newRanges.add(currentRange);
         i += 1;
-        continue;
       }
-
-      // Iterate through all emoji ranges before the end of the current range, splitting the current range at each intersection.
-      while (j < emojiRanges.size()) {
-        MarkdownRange emojiRange = emojiRanges.get(j);
-        if (emojiRange.getStart() > currentRange.getEnd()) {
-          break;
-        }
-
-        int currentStart = currentRange.getStart();
-        int currentEnd = currentRange.getEnd();
-        int emojiStart = emojiRange.getStart();
-        int emojiEnd = emojiRange.getEnd();
-        if (emojiStart >= currentStart && emojiEnd <= currentEnd) { // Intersection
-          MarkdownRange newRange = new MarkdownRange(currentRange.getType(), currentStart, emojiStart - currentStart, currentRange.getDepth());
-          currentRange = new MarkdownRange(currentRange.getType(), emojiEnd, currentEnd - emojiEnd, currentRange.getDepth());
-
-          if (newRange.getLength() > 0) {
-           newRanges.add(newRange);
-          }
-        }
-        j += 1;
-      }
-      newRanges.add(currentRange);
-      i += 1;
+    } finally {
+      Systrace.endSection(0);
     }
-    Systrace.endSection(0);
+
     return newRanges;
   }
 }
