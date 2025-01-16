451a308a360f3ba16ab4def8af6933eb64eff73a
Michał Skałka <michal.skalka@swmansion.com>
2025-01-16 09:49:35 +0100

    Remove example content changes

--- example/src/testConstants.ts
+++ example/src/testConstants.ts
@@ -1,6 +1,18 @@
 const LOCAL_URL = 'http://localhost:19006/';
 
-const EXAMPLE_CONTENT = ['~_😀🍕🍔_~'].join('\n');
+const EXAMPLE_CONTENT = [
+  'Hello, *world*!',
+  '😀🍕🍔',
+  'https://expensify.com',
+  '# header1',
+  '> blockquote',
+  '`inline code`',
+  '```\ncodeblock\n```',
+  '@here',
+  '@someone@swmansion.com',
+  '#mention-report',
+  '![demo image](https://picsum.photos/id/1067/200/300)',
+].join('\n');
 
 const INPUT_ID = 'MarkdownInput_Example';
 const INPUT_HISTORY_DEBOUNCE_TIME_MS = 150;
