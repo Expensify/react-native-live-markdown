64f781e5baa31712606d737136f2676c43f78283
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-01-30 20:47:34 +0100

    Call `setTextKeepState` instead of `setText` and `setSelection`

--- android/src/main/java/com/expensify/livemarkdown/MarkdownTextInputDecoratorView.java
+++ android/src/main/java/com/expensify/livemarkdown/MarkdownTextInputDecoratorView.java
@@ -94,10 +94,7 @@ public class MarkdownTextInputDecoratorView extends View {
 
   protected void applyNewStyles() {
     if (mReactEditText != null) {
-      int selectionStart = mReactEditText.getSelectionStart();
-      int selectionEnd = mReactEditText.getSelectionEnd();
-      mReactEditText.setText(mReactEditText.getText()); // trigger update
-      mReactEditText.setSelection(selectionStart, selectionEnd);
+      mReactEditText.setTextKeepState(mReactEditText.getText()); // trigger update
     }
   }
 }
