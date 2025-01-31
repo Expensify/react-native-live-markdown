88fbe19755494004ad03eb2d9e0603f4b7c2acac
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-01-31 15:42:17 +0100

    Remove leftover `AssetManager` import (#606)

--- android/src/main/java/com/expensify/livemarkdown/MarkdownTextInputDecoratorView.java
+++ android/src/main/java/com/expensify/livemarkdown/MarkdownTextInputDecoratorView.java
@@ -3,7 +3,6 @@ package com.expensify.livemarkdown;
 import androidx.annotation.Nullable;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 
