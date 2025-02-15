6e2151c400456c5cb494cda9f977aeb669d6f7d6
QichenZhu <57348009+QichenZhu@users.noreply.github.com>
2025-02-15 23:25:33 +1300

    Update web example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,6 +4,7 @@ import Animated from 'react-native-reanimated';
 import {
   MarkdownTextInput,
   parseExpensiMark,
+  type MarkdownType,
 } from '@expensify/react-native-live-markdown';
 import * as TEST_CONST from './testConstants';
 import {PlatformInfo} from './PlatformInfo';
@@ -16,49 +17,40 @@ import {PlatformInfo} from './PlatformInfo';
 console.log(Animated);
 
 type FormatRule = {
-  regex: RegExp;
-
-  /** The group number to extract the matched text, excluding the markdown symbols. */
-  matchGroup: number;
+  markdownType: MarkdownType;
+  syntax: string;
 };
 
 function getFormatRule(formatCommand: string): FormatRule | null {
-  // We reuse regexes from expensify-common with modified flags, and they don’t sync automatically.
-  // When expensify-common updates its regexes, we should manually maintain the regex and matchGroup properties here.
   if (formatCommand === 'formatBold') {
-    return {
-      regex: /(?<!<[^>]*)(\b_|\B)\*(?![^<]*(?:<\/pre>|<\/code>|<\/a>|<\/video>))((?![\s*])[\s\S]*?[^\s*](?<!\s))\*\B(?![^<]*>)(?![^<]*(<\/pre>|<\/code>|<\/a>|<\/video>))/dg,
-      matchGroup: 2,
-    };
+    return {markdownType: 'bold', syntax: '*'};
   }
   if (formatCommand === 'formatItalic') {
-    return {
-      regex: /(<(pre|code|a|mention-user|video)[^>]*>(.*?)<\/\2>)|((\b_+|\b)_((?![\s_])[\s\S]*?[^\s_](?<!\s))_(?![^\W_])(?![^<]*>)(?![^<]*(<\/pre>|<\/code>|<\/a>|<\/mention-user>|<\/video>)))/dg,
-      matchGroup: 6,
-    };
+    return {markdownType: 'italic', syntax: '_'};
   }
   return null;
 }
 
 function handleFormatSelection(text: string, selectionStart: number, selectionEnd: number, formatCommand: string) {
   const formatRule = getFormatRule(formatCommand);
-  if (!formatRule) {
+  if (!text || selectionStart == null || selectionEnd == null || !formatRule) {
     return {updatedText: text, cursorOffset: 0};
   }
 
   // Remove formatting if the selection is already formatted.
-  const matches = text.matchAll(formatRule.regex);
-  for (const match of matches) {
-    const [matchStart, matchEnd] = match?.indices?.[formatRule.matchGroup] ?? [];
-    const matchedText = match?.[formatRule.matchGroup];
-    if (matchStart != null && matchEnd != null && matchedText) {
-      const isExactMatch = matchStart === selectionStart && matchEnd === selectionEnd;
-      const isEnclosedMatch = matchStart - 1 === selectionStart && matchEnd + 1 === selectionEnd;
-      if (isExactMatch || isEnclosedMatch) {
-        const prefix = text.slice(0, matchStart - 1);
-        const suffix = text.slice(matchEnd + 1);
-        const updatedText = `${prefix}${matchedText}${suffix}`;
-        const cursorOffset = selectionStart - matchStart - 1;
+  const markdownRanges = parseExpensiMark(text);
+  for (const range of markdownRanges) {
+    if (range && range.type === formatRule.markdownType && range.start != null && range.length != null) {
+      const rangeEnd = range.start + range.length;
+      const isExactMatch = range.start === selectionStart && rangeEnd === selectionEnd;
+      const isEnclosedMatch = range.start - 1 === selectionStart && rangeEnd + 1 === selectionEnd;
+      const isRangeBetweenSyntaxes = text[range.start - 1] === formatRule.syntax && text[rangeEnd] === formatRule.syntax;
+      if ((isExactMatch || isEnclosedMatch) && isRangeBetweenSyntaxes) {
+        const prefix = text.slice(0, range.start - 1);
+        const suffix = text.slice(rangeEnd + 1);
+        const unformattedText = text.slice(range.start, rangeEnd);
+        const updatedText = `${prefix}${unformattedText}${suffix}`;
+        const cursorOffset = selectionStart - range.start - 1;
         return {updatedText, cursorOffset};
       }
     }
@@ -68,7 +60,7 @@ function handleFormatSelection(text: string, selectionStart: number, selectionEn
   const prefix = text.slice(0, selectionStart);
   const suffix = text.slice(selectionEnd);
   const selectedText = text.slice(selectionStart, selectionEnd);
-  const formattedText = formatCommand === 'formatBold' ? `*${selectedText}*` : `_${selectedText}_`;
+  const formattedText = `${formatRule.syntax}${selectedText}${formatRule.syntax}`;
   const updatedText = `${prefix}${formattedText}${suffix}`;
   const cursorOffset = formattedText.length - selectedText.length;
   return {updatedText, cursorOffset};
