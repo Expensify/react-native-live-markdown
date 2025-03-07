83f4cd2a15a46ad11223809132199042cb401ebc
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-03-07 17:16:04 +0100

    Remove old architecture support on Android

--- android/src/oldarch/NativeProxy.java
+++
@@ -1,9 +0,0 @@
-package com.expensify.livemarkdown;
-
-import com.facebook.react.bridge.UIManager;
-
-public class NativeProxy {
-  public void createCommitHook(UIManager uiManager) {
-    // no-op on the old arch
-  }
-}
