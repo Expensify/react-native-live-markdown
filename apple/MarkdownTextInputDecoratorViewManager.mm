dc620f4b3d07efeb4f44f09e7e4dcf760121232b
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-03-10 14:05:59 +0100

    Merge `MarkdownTextInputDecoratorView` into `MarkdownTextInputDecoratorComponentView` on iOS (#636)

--- apple/MarkdownTextInputDecoratorViewManager.mm
+++ apple/MarkdownTextInputDecoratorViewManager.mm
@@ -1,23 +1,11 @@
 #import <RNLiveMarkdown/MarkdownTextInputDecoratorViewManager.h>
-#import <RNLiveMarkdown/MarkdownTextInputDecoratorView.h>
 
 @implementation MarkdownTextInputDecoratorViewManager
 
 RCT_EXPORT_MODULE(MarkdownTextInputDecoratorView)
 
-- (UIView *)view
-{
-  return [[MarkdownTextInputDecoratorView alloc] init];
-}
+RCT_EXPORT_VIEW_PROPERTY(markdownStyle, NSDictionary)
 
-RCT_CUSTOM_VIEW_PROPERTY(markdownStyle, NSDictionary, MarkdownTextInputDecoratorView)
-{
-  // implemented in MarkdownTextInputDecoratorView updateProps:
-}
-
-RCT_CUSTOM_VIEW_PROPERTY(parserId, NSNumber, MarkdownTextInputDecoratorView)
-{
-  // implemented in MarkdownTextInputDecoratorView updateProps:
-}
+RCT_EXPORT_VIEW_PROPERTY(parserId, NSNumber)
 
 @end
