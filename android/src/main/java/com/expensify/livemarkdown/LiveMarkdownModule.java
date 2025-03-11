d99ae35d2588ccf8f4d304bae32180610992f0d0
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-03-11 18:14:08 +0100

    Remove redundant imports

--- android/src/main/java/com/expensify/livemarkdown/LiveMarkdownModule.java
+++ android/src/main/java/com/expensify/livemarkdown/LiveMarkdownModule.java
@@ -1,9 +1,6 @@
 package com.expensify.livemarkdown;
 
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.fabric.FabricUIManager;
-import com.facebook.react.uimanager.UIManagerHelper;
-import com.facebook.react.uimanager.common.UIManagerType;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Objects;
