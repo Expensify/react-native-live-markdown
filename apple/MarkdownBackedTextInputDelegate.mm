bae14a5b4a9a20cb784f58efa92cc9d55f7c19bc
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-03-18 12:03:57 +0100

    Fix cursor height in empty text input with line height set

--- apple/MarkdownBackedTextInputDelegate.mm
+++ apple/MarkdownBackedTextInputDelegate.mm
@@ -29,10 +29,17 @@
   // After adding a newline at the end of the blockquote, the typing attributes in the next line still contain
   // NSParagraphStyle with non-zero firstLineHeadIndent and headIntent added by `_updateTypingAttributes` call.
   // This causes the cursor to be shifted to the right instead of being located at the beginning of the line.
-  // The following code removes NSParagraphStyle from typing attributes to fix the position of the cursor.
-  NSMutableDictionary *typingAttributes = [_textView.typingAttributes mutableCopy];
-  [typingAttributes removeObjectForKey:NSParagraphStyleAttributeName];
-  _textView.typingAttributes = typingAttributes;
+  // The following code resets firstLineHeadIndent and headIndent in NSParagraphStyle in typing attributes
+  // in order to fix the position of the cursor.
+  NSDictionary<NSAttributedStringKey, id> *typingAttributes = _textView.typingAttributes;
+  if (typingAttributes[NSParagraphStyleAttributeName] != nil) {
+    NSMutableDictionary *mutableTypingAttributes = [typingAttributes mutableCopy];
+    NSMutableParagraphStyle *mutableParagraphStyle = [typingAttributes[NSParagraphStyleAttributeName] mutableCopy];
+    mutableParagraphStyle.firstLineHeadIndent = 0;
+    mutableParagraphStyle.headIndent = 0;
+    mutableTypingAttributes[NSParagraphStyleAttributeName] = mutableParagraphStyle;
+    _textView.typingAttributes = mutableTypingAttributes;
+  }
 }
 
 // Delegate all remaining calls to the original text input delegate
