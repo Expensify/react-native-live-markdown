b89f903f4766ffd1533e5335d262339c0c63e22f
Jakub Piasecki <jakubpiasecki67@gmail.com>
2025-03-25 10:19:08 +0100

    Replace the commit hook with a wrapper component based on `display: contents` (#566)
    Co-authored-by: Tomek Zawadzki <tomekzawadzki98@gmail.com>

--- android/src/main/java/com/expensify/livemarkdown/MarkdownTextInputDecoratorViewManagerSpec.java
+++ android/src/main/java/com/expensify/livemarkdown/MarkdownTextInputDecoratorViewManagerSpec.java
@@ -1,15 +1,15 @@
 package com.expensify.livemarkdown;
 
-import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 
-import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ViewGroupManager;
 import com.facebook.react.uimanager.ViewManagerDelegate;
 import com.facebook.react.viewmanagers.MarkdownTextInputDecoratorViewManagerDelegate;
 import com.facebook.react.viewmanagers.MarkdownTextInputDecoratorViewManagerInterface;
 
-public abstract class MarkdownTextInputDecoratorViewManagerSpec<T extends View> extends SimpleViewManager<T> implements MarkdownTextInputDecoratorViewManagerInterface<T> {
+public abstract class MarkdownTextInputDecoratorViewManagerSpec<T extends ViewGroup> extends ViewGroupManager<T> implements MarkdownTextInputDecoratorViewManagerInterface<T> {
   private final ViewManagerDelegate<T> mDelegate;
 
   public MarkdownTextInputDecoratorViewManagerSpec() {
