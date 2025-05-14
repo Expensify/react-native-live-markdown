474c5abc4e14c9fe2ba52608086897925195bb49
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-05-14 23:14:50 +0200

    Migrate `LiveMarkdownModule` to `TurboModuleWithJSIBindings`

--- android/src/main/cpp/OnLoad.cpp
+++ android/src/main/cpp/OnLoad.cpp
@@ -1,14 +1,11 @@
 #include <fbjni/fbjni.h>
 
+#include "LiveMarkdownModule.h"
 #include "MarkdownParser.h"
-#include "RuntimeDecorator.h"
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
-    return facebook::jni::initialize(
-            vm, [] { expensify::livemarkdown::MarkdownParser::registerNatives(); });
-}
-
-extern "C" JNIEXPORT void JNICALL Java_com_expensify_livemarkdown_LiveMarkdownModule_injectJSIBindings(JNIEnv *env, jobject thiz, jlong jsiRuntime) {
-  jsi::Runtime &rt = *reinterpret_cast<jsi::Runtime*>(jsiRuntime);
-  expensify::livemarkdown::injectJSIBindings(rt);
+    return facebook::jni::initialize(vm, [] {
+        expensify::livemarkdown::LiveMarkdownModule::registerNatives();
+        expensify::livemarkdown::MarkdownParser::registerNatives();
+    });
 }
