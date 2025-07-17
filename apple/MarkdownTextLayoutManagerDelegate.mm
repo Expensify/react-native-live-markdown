fbbc20179c48c28ad3da14fca2a19ee3edf0a8df
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-07-17 11:51:32 +0200

    Use two spaces instead of four spaces for indentation

--- apple/MarkdownTextLayoutManagerDelegate.mm
+++ apple/MarkdownTextLayoutManagerDelegate.mm
@@ -6,17 +6,17 @@
 
 - (NSTextLayoutFragment *)textLayoutManager:(NSTextLayoutManager *)textLayoutManager textLayoutFragmentForLocation:(id <NSTextLocation>)location inTextElement:(NSTextElement *)textElement
 API_AVAILABLE(ios(15.0)){
-    NSInteger index = [textLayoutManager offsetFromLocation:textLayoutManager.documentRange.location toLocation:location];
-    if (index < self.textStorage.length) {
-        NSNumber *depth = [self.textStorage attribute:RCTLiveMarkdownBlockquoteDepthAttributeName atIndex:index effectiveRange:nil];
-        if (depth != nil) {
-            BlockquoteTextLayoutFragment *textLayoutFragment = [[BlockquoteTextLayoutFragment alloc] initWithTextElement:textElement range:textElement.elementRange];
-            textLayoutFragment.markdownUtils = _markdownUtils;
-            textLayoutFragment.depth = [depth unsignedIntValue];
-            return textLayoutFragment;
-        }
+  NSInteger index = [textLayoutManager offsetFromLocation:textLayoutManager.documentRange.location toLocation:location];
+  if (index < self.textStorage.length) {
+    NSNumber *depth = [self.textStorage attribute:RCTLiveMarkdownBlockquoteDepthAttributeName atIndex:index effectiveRange:nil];
+    if (depth != nil) {
+      BlockquoteTextLayoutFragment *textLayoutFragment = [[BlockquoteTextLayoutFragment alloc] initWithTextElement:textElement range:textElement.elementRange];
+      textLayoutFragment.markdownUtils = _markdownUtils;
+      textLayoutFragment.depth = [depth unsignedIntValue];
+      return textLayoutFragment;
     }
-    return [[NSTextLayoutFragment alloc] initWithTextElement:textElement range:textElement.elementRange];
+  }
+  return [[NSTextLayoutFragment alloc] initWithTextElement:textElement range:textElement.elementRange];
 }
 
 @end
