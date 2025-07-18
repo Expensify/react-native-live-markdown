dae36b4909086cec8e6edef69bf818f8668668f7
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2025-07-18 09:45:06 +0200

    Revert changes in .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,7 @@ module.exports = {
   tabWidth: 2,
   singleQuote: true,
   trailingComma: 'all',
+  bracketSpacing: false,
   arrowParens: 'always',
   printWidth: 190,
   singleAttributePerLine: true,
