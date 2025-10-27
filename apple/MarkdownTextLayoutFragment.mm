cbab2528e54f34a71770d29d8458075705609c92
war-in <war-in@users.noreply.github.com>
2025-10-27 17:11:45 +0100

    fix: rounded background issues on singeline input when mentions were off screen

--- apple/MarkdownTextLayoutFragment.mm
+++ apple/MarkdownTextLayoutFragment.mm
@@ -17,10 +17,10 @@
     [super drawAtPoint:point inContext:ctx];
     return;
   }
-  
+
   [self drawBlockquoteRibbons];
   [self drawMentions];
-  
+
   [super drawAtPoint:point inContext:ctx];
 }
 
@@ -30,14 +30,14 @@
   if (self.depth == 0) {
     return;
   }
-  
+
   CGFloat marginLeft = _markdownUtils.markdownStyle.blockquoteMarginLeft;
   CGFloat borderWidth = _markdownUtils.markdownStyle.blockquoteBorderWidth;
   CGFloat paddingLeft = _markdownUtils.markdownStyle.blockquotePaddingLeft;
   CGFloat shift = marginLeft + borderWidth + paddingLeft;
-  
+
   [_markdownUtils.markdownStyle.blockquoteBorderColor setFill];
-  
+
   CGRect boundingRect = self.boundingRect;
   for (NSUInteger level = 0; level < _depth; ++level) {
     CGFloat x = boundingRect.origin.x + level * shift;
@@ -50,29 +50,43 @@
   if (self.mentions.count == 0) {
     return;
   }
-  
+
+  bool isSingleline = [self.textLineFragments count] == 1;
   [self.textLineFragments enumerateObjectsUsingBlock:^(NSTextLineFragment * _Nonnull lineFragment, NSUInteger idx, BOOL * _Nonnull stop) {
     if (lineFragment.characterRange.length == 0) {
       return;
     }
-    
+
     CGRect lineBounds = lineFragment.typographicBounds;
+    CGPoint lineEndLocation = [lineFragment locationForCharacterAtIndex: lineFragment.characterRange.length];
     for (RCTMarkdownTextBackgroundWithRange *mention in self.mentions) {
       NSRange intersection = NSIntersectionRange(lineFragment.characterRange, mention.range);
       if (intersection.length == 0) {
         continue;
       }
-      
+
       CGPoint startLocation = [lineFragment locationForCharacterAtIndex:intersection.location];
+      if (isSingleline && startLocation.x == 0 && intersection.location > 0) {
+        // singleline: mention starts off screen, no need to draw background
+        continue;
+      }
+
       CGPoint endLocation = [lineFragment locationForCharacterAtIndex:intersection.location + intersection.length];
+      if (isSingleline && (startLocation.x > endLocation.x || (startLocation.x == endLocation.x && intersection.location == 0))) {
+        // singleline: mention is partially visible
+        // 1. starts in the middle, or
+        // 2. starts at the beginning of the line
+        endLocation = lineEndLocation;
+      }
+
       CGFloat width = endLocation.x - startLocation.x;
       CGFloat x = lineBounds.origin.x + startLocation.x;
-      
+
       CGRect backgroundRect = CGRectMake(x,
                                          lineBounds.origin.y,
                                          width,
                                          lineBounds.size.height);
-      
+
       BOOL isStart = (intersection.location == mention.range.location);
       BOOL isEnd = (NSMaxRange(intersection) == NSMaxRange(mention.range));
       UIRectCorner cornersToRound = 0;
@@ -82,14 +96,14 @@
       if (isEnd) {
         cornersToRound |= UIRectCornerTopRight | UIRectCornerBottomRight;
       }
-      
+
       UIBezierPath *linePath = (cornersToRound == 0)
       ? [UIBezierPath bezierPathWithRect:backgroundRect]
       : [UIBezierPath bezierPathWithRoundedRect:backgroundRect
                               byRoundingCorners:cornersToRound
                                     cornerRadii:CGSizeMake(mention.textBackground.borderRadius,
                                                            mention.textBackground.borderRadius)];
-      
+
       [mention.textBackground.color setFill];
       [linePath fill];
     }
@@ -111,15 +125,15 @@
       fragmentTextBounds = CGRectUnion(fragmentTextBounds, lineFragmentBounds);
     }
   }
-  
+
   CGFloat marginLeft = _markdownUtils.markdownStyle.blockquoteMarginLeft;
   CGFloat borderWidth = _markdownUtils.markdownStyle.blockquoteBorderWidth;
   CGFloat paddingLeft = _markdownUtils.markdownStyle.blockquotePaddingLeft;
   CGFloat shift = marginLeft + borderWidth + paddingLeft;
-  
+
   fragmentTextBounds.origin.x -= (paddingLeft + borderWidth) + shift * (_depth - 1);
   fragmentTextBounds.size.width = borderWidth + shift * (_depth - 1);
-  
+
   return fragmentTextBounds;
 }
 
